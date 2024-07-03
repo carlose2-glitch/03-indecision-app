ba401877f7ced40baa5919f8910d8d4162f3f4c2
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-07-02 23:25:59 -0400

    firt commit

---
+++ src/components/my-counter-script/MyCounterScript2.ts
@@ -0,0 +1,17 @@
+import { useCounter } from '@/composables/userCounter';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    value: { type: Number, required: true },
+  },
+  setup(props) {
+    const { counter, squareCounter } = useCounter(props.value);
+    // const counter = ref(props.value);
+    //const squareCounter = computed(() => counter.value * counter.value);
+
+    return {
+      counter,
+      squareCounter,
+    };
+  },
+});
